cb590ba2facc51333df6760d442cc426a62ec4ec
Yurii Heiko <yurii.heiko@sparrowacoustics.com>
2023-09-28 17:37:06 -0300

    reformat code

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ import logging
 import os
 import smtplib
 from datetime import datetime, timedelta
-from email.mime.application import MIMEApplication  # Added this import
+from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import time
@@ -25,35 +25,25 @@ smtp_username = config.get('SMTP', 'username')
 smtp_password = config.get('SMTP', 'app_password')
 email_subject = config.get('Email', 'subject')
 email_body = config.get('Email', 'body')
-attachment_path = config.get('Email', 'attachment_path')  # Extract attachment_path from config
-attachment_type = config.get('Email', 'attachment_type')  # Extract attachment_path from config
-start_row = int(config.get('GoogleSheet', 'start_row'))  # Extract start_row as an integer
-email_column = int(config.get('GoogleSheet', 'email_column'))  # Extract email_column as an integer
-undelivered_column = int(config.get('GoogleSheet', 'undelivered_column'))  # Extract undelivered_column as an integer
-send_text = config.get('GoogleSheet', 'send_text')  # Extract send_text
-undelivered_text = config.get('GoogleSheet', 'undelivered_text')  # Extract send_text
-sheet_name = config.get('GoogleSheet', 'sheet_name')  # Extract sheet_name
-
-# Define a custom log file path
+attachment_path = config.get('Email', 'attachment_path')
+attachment_type = config.get('Email', 'attachment_type')
+start_row = int(config.get('GoogleSheet', 'start_row'))
+email_column = int(config.get('GoogleSheet', 'email_column'))
+undelivered_column = int(config.get('GoogleSheet', 'undelivered_column'))
+send_text = config.get('GoogleSheet', 'send_text')
+undelivered_text = config.get('GoogleSheet', 'undelivered_text')
+sheet_name = config.get('GoogleSheet', 'sheet_name')
 log_file_path = 'log.txt'
 
 
 # Function to clean up old log entries (older than 7 days)
 def clean_up_old_logs(log_file_path, days_to_keep=7):
     try:
-        # Calculate the date 7 days ago from today
         cutoff_date = datetime.now() - timedelta(days=days_to_keep)
-
-        # Check if the log file exists before attempting to clean it
         if os.path.exists(log_file_path):
-            # Read the existing log file
             with open(log_file_path, 'r') as log_file:
                 lines = log_file.readlines()
-
-            # Filter log entries to keep only those within the last 7 days
             recent_lines = [line for line in lines if parse_log_entry_date(line) >= cutoff_date]
-
-            # Write the filtered log entries back to the log file
             with open(log_file_path, 'w') as log_file:
                 log_file.writelines(recent_lines)
     except Exception as e:
@@ -63,7 +53,6 @@ def clean_up_old_logs(log_file_path, days_to_keep=7):
 # Function to parse the date from a log entry
 def parse_log_entry_date(log_entry):
     try:
-        # Assuming the date is at the beginning of the log entry in the format 'YYYY-MM-DD'
         date_str = log_entry.split(' - ', 1)[0]
         return datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
     except ValueError:
@@ -87,29 +76,28 @@ def log_and_print(message, level=logging.INFO):
     print(message)
 
 
-# Define the scope and credentials file (replace 'YOUR_CREDENTIALS.json' with your actual JSON file)
-scope = ['https://spreadsheets.google.com/feeds',
-         'https://www.googleapis.com/auth/drive']
+# Define the scope and credentials file
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 credentials = ServiceAccountCredentials.from_json_keyfile_name('YOUR_CREDENTIALS.json', scope)
 
 # Authenticate with Google Sheets API
 gc = gspread.authorize(credentials)
 
-# Open a specific Google Sheet by title (using the sheet_name from the config file)
+# Open a specific Google Sheet by title
 try:
     spreadsheet = gc.open(sheet_name)
 except gspread.exceptions.SpreadsheetNotFound:
     log_and_print(f"Error: Google Sheet '{sheet_name}' not found.", level=logging.ERROR)
     exit()
 
-# Select a specific worksheet by index (0-based) or by title
+# Select a specific worksheet by index
 worksheet = spreadsheet.get_worksheet(0)
 
 
 # Function to mark email as undelivered in the Google Sheet
 def mark_email_as_undelivered(address):
     found_cell = worksheet.find(address)
-    worksheet.update_cell(found_cell.row, undelivered_column, undelivered_text)  # Set the cell text to undelivered_text
+    worksheet.update_cell(found_cell.row, undelivered_column, undelivered_text)
     log_and_print(f"Marked email as undelivered for: {address}")
 
 
@@ -117,14 +105,8 @@ def mark_email_as_undelivered(address):
 try:
     imap_server = imaplib.IMAP4_SSL('imap.gmail.com')
     imap_server.login(smtp_username, smtp_password)
-
-    # Explicitly select the INBOX folder to ensure we are in the SELECTED state
     imap_server.select('INBOX')
-
-    # Calculate the date 7 days ago from today
     seven_days_ago = (datetime.now() - timedelta(days=7)).strftime('%d-%b-%Y')
-
-    # Search for undelivered emails from mailer-daemon@googlemail.com within the last 7 days
     search_criteria = f'(UNSEEN FROM "mailer-daemon@googlemail.com" SINCE {seven_days_ago})'
     _, email_ids = imap_server.search(None, search_criteria)
 
@@ -133,7 +115,6 @@ try:
         raw_email = email_data[0][1]
         parsed_email = email.message_from_bytes(raw_email)
 
-        # Check if the email body contains the specific text
         email_body = None
         if parsed_email.is_multipart():
             for part in parsed_email.walk():
@@ -142,14 +123,10 @@ try:
                     break
 
         if email_body and "Your message wasn't delivered" in email_body:
-            # Extract the wrong email address from the email body
             start_index = email_body.find("to ") + 3
             end_index = email_body.find(" because")
             wrong_email_address = email_body[start_index:end_index]
-
-            # Mark the wrong email address as undelivered
             mark_email_as_undelivered(wrong_email_address)
-
             log_and_print(f"Marked email as undelivered for: {wrong_email_address}")
 
     imap_server.logout()
@@ -157,10 +134,9 @@ try:
 except Exception as e:
     log_and_print(f"Error checking mailbox: {str(e)}", level=logging.ERROR)
 
-
 # Get all values from the specified email_column starting from the specified start_row
 column_e_values = worksheet.col_values(email_column)
-undelivered_values = worksheet.col_values(undelivered_column)  # Get values from undelivered_column
+undelivered_values = worksheet.col_values(undelivered_column)
 
 # Create a list of email addresses that have an empty value in the undelivered_column
 filtered_values = []
@@ -199,6 +175,6 @@ for row_number, email_address in enumerate(filtered_values, start=start_row):
 
         # Update the cell with the send_text when email is sent
         cell = worksheet.find(email_address)
-        worksheet.update_cell(cell.row, undelivered_column, send_text)  # Set the cell text to send_text
+        worksheet.update_cell(cell.row, undelivered_column, send_text)
     except Exception as e:
         log_and_print(f"Failed to send email to {email_address} (Row {row_number}): {str(e)}", level=logging.ERROR)
